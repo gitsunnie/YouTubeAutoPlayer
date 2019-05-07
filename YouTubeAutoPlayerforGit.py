b87fb7613f9d3c0a9efb56beb51b1e99360decf1
Shinya <goosunnie@gmail.com>
2019-05-07 17:15:34 +0900

    'test'

--- YouTubeAutoPlayerforGit.py
+++ YouTubeAutoPlayerforGit.py
@@ -1,8 +1,70 @@
 ﻿# -*- coding: utf-8 -*-
 
+# PyDrive での対象ファイルは Root > PythonWorks > Git > WeatherExtractor
+# PyDrive での対象ファイルは Root > PythonWorks > ' Result Stock > AutoYouTubeResult.csv
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
 from selenium import webdriver
 import time
 import socket
+
+import re
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+import datetime
+import csv
+
+
+def auth_google():
+    # Google Oauth 認証を行う
+    gauth = GoogleAuth()
+
+    gauth.LoadCredentialsFile('credentials.json')
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        # gauth.LocalWebserverAuth()
+        gauth.CommandLineAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile('credentials.json')
+
+    drive = GoogleDrive(gauth)
+
+    """
+    # Git > WeatherExtractor 内のファイル一覧を ID と共に表示
+    drive_folder_id = '16k1-Ok7OHFjSPtMkvuVEoBmLUBqlKH9i'
+    query = '"{0}" in parents and trashed=false'.format(drive_folder_id)
+    file_list = drive.ListFile({'q': query}).GetList()
+    for file1 in file_list:
+        print('title: %s, id: %s' % (file1['title'], file1['id']))
+        print(file1)
+    """
+
+    # WeatherResult.csv を操作してみる
+    drive_folder_id = '16k1-Ok7OHFjSPtMkvuVEoBmLUBqlKH9i'
+    query = '"{0}" in parents and trashed=false'.format(drive_folder_id)
+    file_list = drive.ListFile({'q': query}).GetList()
+    for file in file_list:
+        if file['title'] == 'AutoYouTubeResult.csv':  # WeatherResult.csv を取得してダウンロードする
+            content = file.GetContentString()
+            file2 = file
+            # print(file2)
+            break
+
+    file2.GetContentFile('a.csv')  # csv ファイルとしてダウンロード
+    file2.Trash()  # WeatherResult.csv をゴミ箱に移動
+    file2.UnTrash()  # ゴミ箱の外に移動?
+    file2.Delete()  # Hard Delete
+
+    return drive
 
 
 def auto():
@@ -10,11 +72,42 @@ def auto():
     opt.add_argument('--headless')
     opt.binary_location = '/app/.apt/usr/bin/google-chrome'  # Heroku ではバイナリを指定する
     driver_path = '/app/.chromedriver/bin/chromedriver'  # Heroku における driver の位置
+    # driver = webdriver.Chrome('../chromedriver-73.exe', options=opt)
     driver = webdriver.Chrome(executable_path=driver_path, chrome_options=opt)
+
+    """
+    driver = webdriver.Chrome('../chromedriver-73.exe')
+    """
 
     driver.get('https://www.youtube.com/watch?v=s1sHeQnPu90')
 
     time.sleep(15)
+
+
+# YouTube の動画ページにアクセスして 視聴回数を抜き出す
+# 本当は上の auto 関数から視聴回数を抜き出した方が効率がいいけど、追記するのがめんどくさかったのでこちらでやった
+def youtube_search():
+    url = 'https://www.youtube.com/watch?v=s1sHeQnPu90'
+
+    # urlopen() でデータを取得
+    res = req.urlopen(url)
+
+    # BeautifulSoup オブジェクトの作成
+    soup = BeautifulSoup(res, "html.parser")
+
+    # 任意のデータを抽出
+    # views = soup.select('#count')
+    # views = soup.select('span.view-count style-scope yt-view-count-renderer')
+    views_content = soup.select_one('div.watch-view-count')
+
+    # print(views.string)
+    # print(soup)
+
+    pattern = '([0-9.]*)' + ' ' + '([回視聴]*)'
+    a = re.search(pattern, views_content.string)
+    views = a.group(1)
+    print(views)
+    return views
 
 
 def get_ip():
@@ -23,8 +116,37 @@ def get_ip():
 
     ip = socket.gethostbyname(host)
     print(ip)
+    return host, ip
+
+
+# csv に書き込み
+def write_csv(views, host, ip):
+    # 現在の時間を取得
+    dt_now = datetime.datetime.now()
+    year = dt_now.year
+    month = dt_now.month
+    day = dt_now.day
+    present_time = dt_now.time()
+
+    with open('a.csv', mode='a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([year, month, day, present_time, host, ip, views])
+
+
+def upload_file(drive):
+    # PythonWorks > ' Result Stock のフォルダ ID
+    file2 = drive.CreateFile({'parents': [{'id': '16k1-Ok7OHFjSPtMkvuVEoBmLUBqlKH9i'}]})
+    file2.SetContentFile('a.csv')
+    file2['title'] = 'AutoYouTubeResult.csv'
+    file2.Upload()
 
 
 if __name__ == '__main__':
+    drive = auth_google()
+
     auto()
-    get_ip()
+    views = youtube_search()
+    ip_tuple = get_ip()
+    write_csv(views=views, host=ip_tuple[0], ip=ip_tuple[1])
+
+    upload_file(drive=drive)
